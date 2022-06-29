ff99ce3514602ed167658877e36cdb14fb98e3a5
Rami Awar <RamiAwar@users.noreply.github.com>
2022-06-30 01:54:50 +0300

    Update setup.sh

--- pet-courses/pet-introduction/setup.sh
+++ pet-courses/pet-introduction/setup.sh
@@ -1,11 +1,10 @@
+cd /var/tmp
+
 wget -q https://go.dev/dl/go1.18.3.linux-amd64.tar.gz
 echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.bashrc
 exec "$SHELL"
 
-cd /var/tmp
 git clone https://github.com/knqyf263/pet
 cd pet
 go build
 sudo cp pet /usr/local/bin
-
-
